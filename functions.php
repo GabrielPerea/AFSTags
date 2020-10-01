5b90e965db4ec747929b902d80e7a96e314d7d13
Dan Gold <tentativechaos@gmail.com>
2020-10-01 13:43:55 -0700

    Modified new site functions.php with enqueues for tagtree assets.

--- functions.php
+++ functions.php
@@ -12,6 +12,12 @@ add_action( 'wp_enqueue_scripts', 'FLChildTheme::enqueue_scripts', 1000 );
 
 function afs4kids_scripts() {
 	wp_enqueue_script( 'afs4kids-scripts', get_stylesheet_directory_uri() . '/js/afs4kids.js', array('jquery'), '1.0.0', true );
+
+	// Load page specific CSS & JS for tagtree page only if we're actually on a tagtree page
+	if (is_page_template('tagtree.php')) {
+		wp_enqueue_style('tagtree-css', get_stylesheet_directory_uri() . '/css/tagtree.css');
+		wp_enqueue_script('tagtree-js', get_stylesheet_directory_uri() . '/js/tagtree.js');
+	};
 }
 
 add_action( 'wp_enqueue_scripts', 'afs4kids_scripts' );
